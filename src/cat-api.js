ce7433fd2281292885762b63aeec5a38507c73e2
AnnaOsadchuk <119502034+AnnaOsadchuk@users.noreply.github.com>
2023-05-29 09:01:46 +0200

    writing code

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,1 +1,22 @@
+const BASE_URL = 'https://api.thecatapi.com/v1';
+// const API_KEY =
+//   'live_cqc4WO0R0arjDsXazePzOjsOOy0evFB8pHbbpLI2fQRxLXEXEB0UVKfDjFamGZWp';
 
+function fetchBreeds(breedId = undefined) {
+  const URL = `${BASE_URL}/breeds/${breedId !== undefined ? breedId : ''}`;
+  return fetch(URL)
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => console.log(error));
+}
+
+function fetchCatByBreed(breedId) {
+  return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => console.log(error));
+}
+
+export default { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,69 @@
 import './css/styles.css';
+import CatApi from './cat-api';
+// import  catCardContainer from'./cat-cards.hbs';
+const breedSelectEl = document.querySelector('.breed-select');
+const cardContainer = document.querySelector('.cat-info');
+breedSelectEl.addEventListener('change', onSelect);
 
-const DEBOUNCE_DELAY = 300;
-
-const URL = 'https://api.thecatapi.com/v1/breed';
-const API_KEY =
-  'live_cqc4WO0R0arjDsXazePzOjsOOy0evFB8pHbbpLI2fQRxLXEXEB0UVKfDjFamGZWp';
-let arrayCats = [];
-
-// fetchBreeds() {
-//     return fetch(`${URL}?limit=1&api_key=${API_KEY}`).then(response => {
-//         return response.json();
-//     });
-// };
-
-fetch(URL, { headers: { 'x-api-kay': API_KEY } })
-  .then(response => {
-    return response.json();
-  })
-  .then(cat => {
-    console.log(cat);
-  })
-  .catch(error => {
-    console.log(error);
+CatApi.fetchBreeds().then(breeds => {
+  breeds.map(breed => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.text = breed.name;
+    breedSelectEl.add(option);
   });
+});
+
+function onSelect(event) {
+  const breedId = event.target.value;
+  CatApi.fetchCatByBreed(breedId).then(image => {
+    // Get image URL
+    const imageUrl = image[0].url;
+    // Fetch other breed information
+    CatApi.fetchBreeds(breedId).then(breed => {
+      const breedInfo = {
+        name: breed.name,
+        description: breed.description,
+        temperament: breed.temperament,
+      };
+      cardContainer.innerHTML = createMarkup(imageUrl, breedInfo);
+    });
+  });
+}
+
+function createMarkup(imageUrl, breedInfo) {
+  return `<img
+        src="${imageUrl}"
+        alt="${breedInfo.name}"
+        width="400"
+      />
+      <div class="infromation">
+        <h2>${breedInfo.name}</h2>
+        <p class="text">${breedInfo.description}</p>
+        <p class="text"><b>Temperament:</b> ${breedInfo.temperament}</p>
+      </div>`;
+}
+
+// const DEBOUNCE_DELAY = 300;
+// const refs = {
+//   cardContainer: document.querySelector('.cat-info');
+// }
+
+// function fetchBreeds(query) {
+// fetch(URL, { headers: { 'x-api-kay': API_KEY } })
+//   .then(response => {
+//     return response.json();
+//   })
+
+// .then(cat => {
+//   console.log(cat);
+//   const markup = catCardContainer(cat);
+//   console.log(markup);
+//   refs.cardContainer.innerHTML =
+//
+// })
+// .catch(error => {
+//   console.log(error);
+// });
+// }
+// fetchBreeds();
