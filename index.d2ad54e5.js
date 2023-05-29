09f7431f02a3531d1377d793c620c22d62d93ddd
AnnaOsadchuk <119502034+AnnaOsadchuk@users.noreply.github.com>
2023-05-29 07:03:08 +0000

    Deploying to gh-pages from @ AnnaOsadchuk/goit-js-hw-11@ce7433fd2281292885762b63aeec5a38507c73e2 🚀

--- index.d2ad54e5.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breed",{headers:{"x-api-kay":"live_cqc4WO0R0arjDsXazePzOjsOOy0evFB8pHbbpLI2fQRxLXEXEB0UVKfDjFamGZWp"}}).then((function(e){return e.json()})).then((function(e){console.log(e)})).catch((function(e){console.log(e)}));
-//# sourceMappingURL=index.d2ad54e5.js.map
